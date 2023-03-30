36b807691c62ec943b4d7544b49baa21a5e77e04
Kridtity Lawang <worklawang@gmail.com>
2023-03-30 17:03:06 +0800

    Added initial commit to repo' git commit -m 'Added initial commit to repo'
    exit
    clear

---
+++ README.md
@@ -0,0 +1,2 @@
+# omhospital
+Repo for custom odoo practice addon/module for fictional hospital OM Hospital
---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'OM Hospital Management System',
+    'version' : '1.0.0',
+    'summary': 'Hospital management system',
+    'description': """Hospital management system""",
+    'category': 'Hospital',
+    'website': '',
+    'images' : [],
+    'depends' : [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'assets': {},
+    'license': 'LGPL-3',
+}
