7befa9eb9a6c44669c46b19f20e69db7a3989447
Kridtity Lawang <worklawang@gmail.com>
2023-03-30 17:21:53 +0800

    Updated manifest to add author name and change sequence number

--- om_hospital/__manifest__.py
+++ om_hospital/__manifest__.py
@@ -3,6 +3,8 @@
     'name' : 'OM Hospital Management System',
     'version' : '1.0.0',
     'summary': 'Hospital management system',
+    'author': 'Kridtity Lawang',
+    'sequence': '-1000',
     'description': """Hospital management system""",
     'category': 'Hospital',
     'website': '',
