be6485807eb41c901561028d88a19a7c73e56e54
Kridtity Lawang <worklawang@gmail.com>
2023-03-31 09:52:37 +0800

    Added basic fields to patient model

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -4,3 +4,7 @@ from odoo import api, fields, models, _
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Hospital Patient'
+
+    name = fields.Char(string = 'Name')
+    age = fields.Integer(string = 'Age')
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string = 'Gender')
